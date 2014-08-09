6d5108b1a6c7be1f1840d2c753e846c48b584467
Henry Lee <manageyp@gmail.com>
2014-08-09 14:29:37 +0800

    [NewFeature] users login,logout,register,forget_password pages

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+  before_filter :config_userpage
+
+  def login
+  end
+
+  def do_login
+  end
+
+  def logout
+  end
+
+  def register
+  end
+
+  def do_register
+
+  end
+
+  def forget_password
+  end
+
+  def do_forget_password
+  end
+
+  def reset_password
+
+  end
+
+  def send_captcha
+
+  end
+
+  private
+  def config_userpage
+    @home_page = true
+  end
+end
